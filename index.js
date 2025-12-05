75d34c37839240a7bf7555e0af5f11b045cbc812
Preetham1869 <23mh1a05l1@acoe.edu.in>
2025-12-05 11:27:57 +0530

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
-import http from 'http';
-import { json } from 'stream/consumers';
+// import http from 'http';
+// import { json } from 'stream/consumers';
 
 
-const server = http.createServer((req, res) => {
-    console.log(req.meathod);
-    if (req.url === '/main') {
-        res.writeHead(200, { 'content-type': 'application/json' });
-        res.end(JSON.stringify({message: "Its a fake main page nothing is going on here" }));
-    }
-    else if (req.url === '/about') {
-        res.writeHead(200, { 'content-type': 'application/json' });
-        res.end(JSON.stringify({ message: "There is no about regarding about me in this page" }));
-    }
-    else if (req.url === '/contact') {
-        res.writeHead(200, { 'content-type': 'application/json' });
-        res.end(JSON.stringify({  message: "Don't contact me" }));
-    }
-    else if (req.url === '/info') {
-        res.writeHead(200, { 'content-type': 'application/json' });
-        res.end(JSON.stringify({  message: "if you want any information go to Google" }));
-    }
-    else {
-        res.writeHead(200, { 'content-type': 'text/plain' });
-        res.end("Why u came here go back to IDE and do your work");
-    }
+// const server = http.createServer((req, res) => {
+//     console.log(req.meathod);
+//     if (req.url === '/main') {
+//         res.writeHead(200, { 'content-type': 'application/json' });
+//         res.end(JSON.stringify({message: "Its a fake main page nothing is going on here" }));
+//     }
+//     else if (req.url === '/about') {
+//         res.writeHead(200, { 'content-type': 'application/json' });
+//         res.end(JSON.stringify({ message: "There is no about regarding about me in this page" }));
+//     }
+//     else if (req.url === '/contact') {
+//         res.writeHead(200, { 'content-type': 'application/json' });
+//         res.end(JSON.stringify({  message: "Don't contact me" }));
+//     }
+//     else if (req.url === '/info') {
+//         res.writeHead(200, { 'content-type': 'application/json' });
+//         res.end(JSON.stringify({  message: "if you want any information go to Google" }));
+//     }
+//     else {
+//         res.writeHead(200, { 'content-type': 'text/plain' });
+//         res.end("Why u came here go back to IDE and do your work");
+//     }
+// });
+// server.listen(9000, () => {
+//     console.log(`Server is running at port ${9000}`);
+// });
+
+
+
+import express from 'express';
+const app= express();
+app.get('/',(req,res) => {
+    res.send("Hello from Express server but nothing is here");
 });
-server.listen(9000, () => {
+app.get('/main',(req,res) => {
+    res.send("Its a fake main page nothing is going on here" );
+});
+app.get('/about',(req,res) => {
+    res.send("There is no about regarding about me in this page" );
+}); 
+app.get('/contact',(req,res) => {
+    res.send("Don't contact me" );
+});
+app.get('/info',(req,res) => {
+    res.send("if you want any information go to Google" ); 
+});
+app.listen(9000, () => {
     console.log(`Server is running at port ${9000}`);
 });
