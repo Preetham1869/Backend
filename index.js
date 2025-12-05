969ff64989b019bf68a1f27a3514065ee5f86bb8
Preetham1869 <23mh1a05l1@acoe.edu.in>
2025-12-05 10:37:19 +0530

    Refactor request handling in server to include additional routes

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ import { json } from 'stream/consumers';
 
 const server = http.createServer((req, res) => {
     console.log(req.meathod);
-    if (req.url === '/54') {
+    if (req.url === '/main') {
         res.writeHead(200, { 'content-type': 'application/json' });
         res.end(JSON.stringify({ name: "Nothing" }));
     }
-    res.writeHead(200, { 'content-type': 'text/plain' });
-    res.end("Backend Server");
+    else if (req.url === '/about') {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify({ name: "About Page" }));
+    }
+    else if (req.url === '/contact') {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify({ name: "Contact Page" }));
+    }
+    else if (req.url === '/info') {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify({ name: "Info Page" }));
+    }
+    else {
+        res.writeHead(200, { 'content-type': 'text/plain' });
+        res.end("Backend Server");
+    }
 });
 server.listen(9000, () => {
     console.log(`Server is running at port ${9000}`);
